a74389e0a7a42ddb8cff212bf4225d67a30a87fa
Jason N <33561705+JasonN3@users.noreply.github.com>
2023-05-30 03:57:02 +0000

    add ipxe

---
+++ src/fcos/fcos.ipxe.php
@@ -0,0 +1,50 @@
+#!ipxe
+<?php
+
+if (isset($_SERVER['HTTPS']) &&
+    ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) ||
+    isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
+    $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+  $protocol = 'https://';
+}
+else {
+  $protocol = 'http://';
+}
+
+echo "set base " . $protocol . $_SERVER['HTTP_HOST'];
+?>
+
+menu Select a channel
+<?php
+
+$stable = json_decode(file_get_contents("https://builds.coreos.fedoraproject.org/streams/stable.json"));
+$stable_version = $stable->architectures->x86_64->artifacts->metal->release;
+echo "item Stable (" . $stable_version . ") stable";
+?>
+choose channel
+
+goto ${channel}
+
+<?php
+echo ":stable";
+echo "set version $stable_version";
+echo "goto os_menu";
+?>
+
+
+:os_menu
+menu Please choose an operating system to deploy
+item fcos Fedora CoreOS
+item fcos-live Fedora CoreOS (Live)
+choose os && goto ${os}
+
+:fcos
+kernel https://builds.coreos.fedoraproject.org/prod/streams/${channel}/builds/${version}/x86_64/fedora-coreos-${version}-live-kernel-x86_64 rd.neednet=1 console=tty0 initrd=main coreos.live.rootfs_url=https://builds.coreos.fedoraproject.org/prod/streams/${channel}/builds/${version}/x86_64/fedora-coreos-${version}-live-rootfs.x86_64.img coreos.inst.install_dev=/dev/sda coreos.inst.ignition_url=${base}/fcos/ignition.php coreos.inst.image_url=https://builds.coreos.fedoraproject.org/prod/streams/${channel}/builds/${version}/x86_64/fedora-coreos-${version}-metal.x86_64.raw.xz coreos.inst.copy-network
+initrd https://builds.coreos.fedoraproject.org/prod/streams/${channel}/builds/${version}/x86_64/fedora-coreos-${version}-live-initramfs.x86_64.img
+boot
+
+:fcos-live
+kernel https://builds.coreos.fedoraproject.org/prod/streams/${channel}/builds/${version}/x86_64/fedora-coreos-${version}-live-kernel-x86_64 ip=dhcp rd.neednet=1 console=tty0 initrd=main coreos.live.rootfs_url=https://builds.coreos.fedoraproject.org/prod/streams/${channel}/builds/${version}/x86_64/fedora-coreos-${version}-live-rootfs.x86_64.img ignition.firstboot ignition.platform.id=metal ignition.config.url=${base}/fcos/ignition.php
+#kernel https://builds.coreos.fedoraproject.org/prod/streams/${channel}/builds/${version}/x86_64/fedora-coreos-${version}-live-kernel-x86_64 ip=dhcp rd.neednet=1 console=tty0 ignition.firstboot ignition.platform.id=metal ignition.config.url=${base}/fcos/ignition.php
+initrd https://builds.coreos.fedoraproject.org/prod/streams/${channel}/builds/${version}/x86_64/fedora-coreos-${version}-live-initramfs.x86_64.img
+boot
