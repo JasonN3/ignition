011c22621f8effa294d5323f3f558de57590b3bf
Jason N <33561705+JasonN3@users.noreply.github.com>
2023-06-02 01:12:39 +0000

    automatically update bootloader

--- src/fcos/configs/zincati.ign.php
+++ src/fcos/configs/zincati.ign.php
@@ -5,8 +5,12 @@ require "customizations/customizations.php";
 $ignition = (object)[];
 $ignition->ignition = (object)[];
 $ignition->ignition->version = "3.3.0";
+
 $ignition->storage = (object)[];
 $ignition->storage->files = [];
+
+$ignition->systemd = (object)[];
+$ignition->systemd->units = [];
 
 if($fleet_lock_server != "") {
     // Create Trusted User CA Key
@@ -22,6 +26,13 @@ if($fleet_lock_server != "") {
     $file->append[] = $file_content;
     $ignition->storage->files[] = $file;
 }
+
+// Add prep-machine service
+$systemd_unit = (object)[];
+$systemd_unit->name = "bootupd-auto.service";
+$systemd_unit->enabled = true;
+$systemd_unit->contents = file_get_contents("files/bootupd-auto.service");
+$ignition->systemd->units[] = $systemd_unit;
 
 print(json_encode($ignition, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
 ?>
