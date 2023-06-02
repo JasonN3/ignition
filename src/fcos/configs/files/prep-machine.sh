8eb17cefe9514c340fb3d6061a7c10ed7a0262df
Jason N <33561705+JasonN3@users.noreply.github.com>
2023-06-02 17:48:33 +0000

    move more configs from prep to ignition

--- src/fcos/configs/files/prep-machine.sh
+++ src/fcos/configs/files/prep-machine.sh
@@ -6,13 +6,8 @@ rpm-ostree install qemu-guest-agent kubeadm
 
 for path in /var/lib/etcd /etc/kubernetes/pki /etc/kubernetes/pki/etcd /etc/cni/net.d
 do
-  mkdir -p $path
-  chcon -t svirt_sandbox_file_t $path
+  chcon -vt svirt_sandbox_file_t $path
 done
-
-mkdir /etc/containerd || true
-containerd config default > /etc/containerd/config.toml
-sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
 
 ln -s /usr/lib/systemd/system/kubelet.service /etc/systemd/system/multi-user.target.wants/kubelet.service
 
--- src/fcos/configs/kubernetes.ign.php
+++ src/fcos/configs/kubernetes.ign.php
@@ -1,5 +1,8 @@
 <?php
 
+require "defaults.php";
+require "customizations/customizations.php";
+
 $ignition = (object)[];
 
 $ignition->ignition = (object)[];
@@ -7,6 +10,7 @@ $ignition->ignition->version = "3.3.0";
 
 $ignition->storage = (object)[];
 $ignition->storage->files = [];
+$ignition->storage->directories = [];
 
 $ignition->systemd = (object)[];
 $ignition->systemd->units = [];
@@ -78,7 +82,87 @@ fs.inotify.max_user_watches         = 524288
 $file->contents->source = "data:," . rawurlencode($content);
 $ignition->storage->files[] = $file;
 
+// Prep kubernetes directories
+$directories = ["/var/lib/etcd", "/etc/kubernetes/pki", "/etc/kubernetes/pki/etcd", "/etc/cni/net.d"];
 
+foreach($directories as $directory) {
+    $dir = (object)[];
+    $dir->path = $directory;
+    $igntion->storage->directories[] = $dir;
+}
+
+// Configure Containerd
+$file = (object)[];
+$file->path = "/etc/containerd/config.toml";
+$file->contents = (object)[];
+$file->contents->compresssion = "";
+$content = "version = 2
+
+imports = [\"/etc/containerd/config.d/*.toml\"]
+";
+$file->contents->source = "data:," . rawurlencode($content);
+$ignition->storage->files[] = $file;
+
+// Configure Containerd to use CGroups
+$file = (object)[];
+$file->path = "/etc/containerd/config.d/cgroups.toml";
+$file->contents = (object)[];
+$file->contents->compresssion = "";
+$content = "version = 2
+
+[plugins.\"io.containerd.grpc.v1.cri\".containerd.runtimes.runc.options]
+  SystemdCgroup = false
+";
+$file->contents->source = "data:," . rawurlencode($content);
+$ignition->storage->files[] = $file;
+
+// Configure caching servers
+$file = (object)[];
+$file->path = "/etc/containerd/config.d/cache.toml";
+$file->contents = (object)[];
+$file->contents->compresssion = "";
+$content = "version = 2
+
+[plugins.\"io.containerd.grpc.v1.cri\".registry]
+  config_path = \"/etc/containerd/certs.d\"
+";
+$file->contents->source = "data:," . rawurlencode($content);
+$ignition->storage->files[] = $file;
+
+foreach($caching_servers as $cache_srv) {
+    $dir = (object)[];
+    $dir->path = "/etc/containerd/certs.d/" . $cache_srv->name;
+    $ignition->storage->directories[] = $dir;
+
+    $file = (object)[];
+    $file->path = "/etc/containerd/certs.d/" . $cache_srv->name . "/hosts.toml";
+    $file->contents = (object)[];
+    $file->contents->compresssion = "";
+    $content = "server = \"" . $cache_srv->server . "\"
+
+[host.\"" . $cache_srv->cache . "\"]
+  capabilities = [\"pull\", \"resolve\"]
+  override_path = true
+";
+    $file->contents->source = "data:," . rawurlencode($content);
+    $ignition->storage->files[] = $file;
+}
+
+// Configure registry authentication
+foreach($registry_auth as $auth) {
+    $file = (object)[];
+    $file->path = "/etc/containerd/config.d/auth_" . $auth->registry . ".toml";
+    $file->contents = (object)[];
+    $file->contents->compresssion = "";
+    $content = "version = 2
+
+[plugins.\"io.containerd.grpc.v1.cri\".registry.configs.\"" . $auth->registry . "\".auth]
+  username = \"" . $auth->username . "\"                          
+  password = \"" . $auth->password . "\"
+";
+    $file->contents->source = "data:," . rawurlencode($content);
+    $ignition->storage->files[] = $file;
+}
 
 print(json_encode($ignition, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
 ?>
