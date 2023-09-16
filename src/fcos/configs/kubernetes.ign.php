7640535993f6aa2654055bbf265bf9521cff1e1d
Jason N <33561705+JasonN3@users.noreply.github.com>
2023-09-16 08:16:50 -0500

    quotes

--- src/fcos/configs/kubernetes.ign.php
+++ src/fcos/configs/kubernetes.ign.php
@@ -119,10 +119,10 @@ $overrides = "version = 2
 
 [plugins]
   [plugins.\"io.containerd.grpc.v1.cri\"]
-    [plugins."io.containerd.grpc.v1.cri".containerd]
-      [plugins."io.containerd.grpc.v1.cri".containerd.runtimes]
-        [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc]
-          [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]
+    [plugins.\"io.containerd.grpc.v1.cri\".containerd]
+      [plugins.\"io.containerd.grpc.v1.cri\".containerd.runtimes]
+        [plugins.\"io.containerd.grpc.v1.cri\".containerd.runtimes.runc]
+          [plugins.\"io.containerd.grpc.v1.cri\".containerd.runtimes.runc.options]
             SystemdCgroup = true
     [plugins.\"io.containerd.grpc.v1.cri\".registry]
       config_path = \"/etc/containerd/certs.d\"
